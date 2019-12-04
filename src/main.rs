3d99945aeab6658ae92fbaeac679d0fc77ebc4f2
James Laverack <james@jameslaverack.com>
2019-12-04 18:40:06 +0000

    Add tests for day 1a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Module {
+    mass: u32
+}
+
+fn fuel_requirement(module: Module) -> u32 {
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fuel_requirement() {
+        assert_eq!(fuel_requirement(Module{mass: 12}), 2);
+        assert_eq!(fuel_requirement(Module{mass: 14}), 2);
+        assert_eq!(fuel_requirement(Module{mass: 1969}), 654);
+        assert_eq!(fuel_requirement(Module{mass: 100756}), 33583);
+    }
+}
