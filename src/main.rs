b92a63141b0a174eee0aa8a303d61895a3efca3c
James Laverack <james@jameslaverack.com>
2019-12-05 18:02:42 +0000

    Handle low mass modules

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ struct Module {
 }
 
 fn fuel_requirement(module: Module) -> u32 {
-    return module.mass / 3 - 2;
+    let mass_div_three = module.mass / 3;
+    return if mass_div_three < 2 { 0 } else { mass_div_three - 2 };
 }
 
 #[cfg(test)]
@@ -20,5 +21,8 @@ mod tests {
         assert_eq!(fuel_requirement(Module{mass: 14}), 2);
         assert_eq!(fuel_requirement(Module{mass: 1969}), 654);
         assert_eq!(fuel_requirement(Module{mass: 100756}), 33583);
+        assert_eq!(fuel_requirement(Module{mass: 2}), 0);
+        assert_eq!(fuel_requirement(Module{mass: 1}), 0);
+        assert_eq!(fuel_requirement(Module{mass: 0}), 0);
     }
 }
