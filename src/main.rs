d74ffc3b257b3c59a847a0b750469a81f5370405
James Laverack <james@jameslaverack.com>
2019-12-07 02:35:46 +0000

    Use clap to add in a cmd to select the day and part to run.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,48 @@
+extern crate clap;
+
 use std::path::Path;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::io;
 use std::error::Error;
 use std::num::ParseIntError;
+use clap::{Arg, App, SubCommand};
 
 fn main() {
+    let matches = App::new("Google Advent of Code 2019 Solver")
+        .version("0.1.0")
+        .author("James Laverack <james@jameslaverack.com>")
+        .about("Code solutions to Google's Advent of Code 2019")
+        .subcommand(SubCommand::with_name("day1a")
+            .about("Day one, part A")
+            .arg(Arg::with_name("MODULES_FILE")
+                .required(true)
+                .index(1)
+                .help("Path to file with module masses")))
+        .get_matches();
+
     println!("Google Advent of Code 2019");
 
-    let file_name = "day1";
-    let file_path = Path::new(file_name);
+    match matches.subcommand_name() {
+        Some("day1a") => {
+            let file_name = matches
+                .subcommand_matches("day1a")
+                .unwrap()
+                .value_of("MODULES_FILE")
+                .unwrap();
+            let file_path = Path::new(file_name);
 
-    println!("Executing day one, part one on file {}", file_path.display());
+            println!("Executing day one, part one on file {}", file_path.display());
 
-    let total_fuel = match total_fuel_requirement(file_path) {
-        Err(why) => panic!("Failed to calculate total fuel requirement: {}", why),
-        Ok(total) => total,
-    };
-    println!("Total fuel requirement is {}", total_fuel)
+            let total_fuel = match total_fuel_requirement(file_path) {
+                Err(why) => panic!("Failed to calculate total fuel requirement: {}", why),
+                Ok(total) => total,
+            };
+            println!("Total fuel requirement is {}", total_fuel)
+        }
+        None => println!("Please use a command"),
+        _ => unreachable!()
+    }
 }
 
 fn total_fuel_requirement(module_masses: &Path) -> Result<u32, Box<dyn Error>> {
